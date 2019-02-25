492d4e38c58c922e56434583996e3f264e7104f9
info@chapatte.co <Info@chapatte.co>
2019-02-25 13:26:02 +0700

    plz

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(res,req) => {
-    res.send({bye: 'buddy'})
+    res.send({bye: 'buddy'});
 });
 const PORT = process.env.PORT ;
 
