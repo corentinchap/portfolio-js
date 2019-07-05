3b8ee5c6c32de8f54dfb6519f2b319271fc86498
Corentin Chapatte <info@chapatte.co>
2019-07-05 19:34:09 +0200

    v1.0

---
+++ client/build/precache-manifest.90e4ae484492f0d177243ca9ad7a4ddc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac601e9ea0e75a1b1ddb9d3d30376dbd",
+    "url": "/static/media/linkedin.ac601e9e.svg"
+  },
+  {
+    "revision": "87e2efc0c8a85ad645e5d457278c2c1b",
+    "url": "/static/media/github.87e2efc0.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5b1e6117862719b7b47c",
+    "url": "/static/js/main.04d2cb02.chunk.js"
+  },
+  {
+    "revision": "44111e2a6ccf1580925f",
+    "url": "/static/js/2.06f0ed30.chunk.js"
+  },
+  {
+    "revision": "5b1e6117862719b7b47c",
+    "url": "/static/css/main.b4ad6878.chunk.css"
+  },
+  {
+    "revision": "44111e2a6ccf1580925f",
+    "url": "/static/css/2.cd50ea80.chunk.css"
+  },
+  {
+    "revision": "d0536525eda3677c64e5de0dce7d6118",
+    "url": "/index.html"
+  }
+];
