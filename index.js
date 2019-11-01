d667510fbc53e3836d24d8910e2e1bed062b5edd
Corentin Chapatte <info@chapatte.co>
2019-11-01 14:42:33 +0100

    Changed CORS Policy and updated NPM

--- index.js
+++ index.js
@@ -37,7 +37,13 @@ app.use(
 );
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    if(process.env.NODE_ENV === 'dev'){
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    }
+    else
+    {
+        res.header("Access-Control-Allow-Origin", "*");
+    }
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
